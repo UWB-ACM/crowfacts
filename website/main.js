eb956285e878b70da51e39b3b76b8a1971eabdf7
etcadinfinitum <lizzy.presland@gmail.com>
2020-03-18 01:10:52 -0700

    Change menu item bg color when selected :rainbow:
    Trying to use as many of the named CSS colors as possible >:]

--- website/main.js
+++ website/main.js
@@ -1,13 +1,20 @@
-function renderPage(divName) {
+function renderPage(divName, menuItem) {
     // hide any visible content divs
     var contentDivs = document.getElementsByClassName('content');
     for (var i = 0; i < contentDivs.length; i++) {
         if (!contentDivs[i].classList.contains('hidden')) {
             contentDivs[i].classList.add('hidden');
-            // DEBUG ONLY
-            console.log('Added hidden: ' + contentDivs[i].outerHTML);
         }
     }
+
+    // make desired div visible
     var target = document.getElementById(divName);
     target.classList.remove('hidden');
+
+    // change menu item background to reflect current selection
+    var menuItems = document.getElementsByClassName('menu-item');
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].classList.remove('active-task');
+    }
+    menuItems[menuItem].classList.add('active-task');
 }
