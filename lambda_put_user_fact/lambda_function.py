9607f6a73cc6112f1347f517c09d91c27a9e59d8
etcadinfinitum <lizzy.presland@gmail.com>
2020-04-09 14:18:09 -0700

    Rework Lambda function to include custom errors

--- lambda_put_user_fact/lambda_function.py
+++ lambda_put_user_fact/lambda_function.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import os
 
 def lambda_handler(event, context):
 	# Create a connection to the DynamoDB instance of AWS
@@ -9,12 +10,25 @@ def lambda_handler(event, context):
 	table = dynamodb.Table('FunFacts')
 	
 	# Set some default responses for the API request
-	response = 'The client request was malformed and required data is missing.'
-	code = 400
+	code = 200
+	response = 'Success!'
 	
 	# Check to make sure the expected data exists in the event body
 	if 'fact' in event and 'source' in event:
 		# Good, we have all the data we need
+		
+		# We should probably check the user's input to make sure it doesn't contain 
+		# any bad words. (This is, after all, a site intended for anyone.)
+		for i in range(1, 13):
+			# Use environment variables
+			word = os.environ['naughty' + str(i)]
+			if word in event['fact'].lower() or word in event['source'].lower():
+				# For logging
+				print('Naughty words detected.')
+				# Return error to API Gateway
+				raise Exception('Thou said a naughty word, and the Crow of Judgement judges thee.')
+				
+		# Input is acceptable.
 		# Now, attempt the put_item() call on the table.
 		try:
 			response = table.put_item(
@@ -29,17 +43,16 @@ def lambda_handler(event, context):
 				# the old data so that we may inform the client.
 				ReturnValues='ALL_OLD'
 			)
-			code = 200
 		except Exception as e:
-			code = 500
-			response = e.response['Error']['Message']
+			raise Exception('Server error:' + e.response['Error']['Message'])
 
 	else:
 		# Uh oh, the client sent a malformed request.
 		# Use the initialized response and code values; do nothing else.
-		pass
+		raise Exception('The client request was malformed and required data is missing.')
 	
 	return {
 		'statusCode': code,
 		'body': response
 	}
+
