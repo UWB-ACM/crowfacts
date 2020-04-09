e777be0cf6770e30b3e6a57d1e9a5c5795d57b4f
etcadinfinitum <lizzy.presland@gmail.com>
2020-04-09 14:19:00 -0700

    Display Lambda function's error message in site

--- website/scripts/dynamo.js
+++ website/scripts/dynamo.js
@@ -73,7 +73,7 @@ function submitUserFact() {
         },
         error: function (response) {
             alert("Unsuccessful");
-            document.getElementById("factRequestPlaceholder").innerHTML = "Your last request was unsuccessful. The error was:\n" + response['body'];
+            document.getElementById("factRequestPlaceholder").innerHTML = "Your last request was unsuccessful. The error was:<br/><strong>" + response['responseJSON']['errorMessage'] + "</strong>";
         }
     });
 }
